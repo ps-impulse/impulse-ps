14cc4e65ec169c0ae1bd3c9f98a33fe50e83fbb8
PrinceSky-Git <princeskygithub@gmail.com>
2026-05-04 22:34:38 +0530

    Add custom formats for PokeRogue in TypeScript

---
+++ config/custom-formats.ts
@@ -0,0 +1,30 @@
+export const Formats: FormatList = [
+	{
+		section: "PokeRogue",
+	},
+	{
+		name: "[Gen 9] PokeRogue",
+		mod: 'pokerogue',
+		ruleset: [
+			'Cancel Mod',
+			'HP Percentage Mod',
+			'PokeRogue Classic',
+		],
+		DefaultLevel: 1,
+		MaxLevel: 999999,
+		banlist: [],
+	},
+	{
+		name: "[Gen 9] PokeRogue Early",
+		mod: 'pokerogue',
+		ruleset: [
+			//'Terastal Clause',
+			'Cancel Mod',
+			'HP Percentage Mod',
+			'PokeRogue Classic',
+		],
+		DefaultLevel: 1,
+		MaxLevel: 999999,
+		banlist: [],
+	},
+];
