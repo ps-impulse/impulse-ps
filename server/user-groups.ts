093baf5edb95e977695edef33eb0d0c50e91be24
PrinceSky-Git <princeskygithub@gmail.com>
2026-05-05 22:41:40 +0530

    Update user-groups.ts

--- server/user-groups.ts
+++ server/user-groups.ts
@@ -2,7 +2,7 @@ import { FS } from '../lib/fs';
 import type { RoomSection } from './chat-commands/room-settings';
 import { toID } from '../sim/dex-data';
 
-export type GroupSymbol = '~' | 'F' | '#' | 'L' | '★' | '*' | '@' | 'O' | '%' | '☆' | '§' | '+' | '^' | ' ' | '‽' | '!';
+export type GroupSymbol = '~' | '√' | '#' | '&' | '★' | '*' | '@' | 'π' | '%' | '☆' | '§' | '+' | '^' | ' ' | '‽' | '!';
 export type EffectiveGroupSymbol = GroupSymbol | 'whitelist';
 export type AuthLevel = EffectiveGroupSymbol | 'unlocked' | 'trusted' | 'autoconfirmed';
 
